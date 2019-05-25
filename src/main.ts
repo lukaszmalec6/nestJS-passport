deb263eb11ce214a331552e3a3b7942b7ca2618a
Lukasz Malec <lukaszmalec6@gmail.com>
2019-05-25 17:07:18 +0200

    apidoc removed

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import {join} from 'path';
 import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  // app.useStaticAssets(join(__dirname, '..', 'public'));
 
   const options = new DocumentBuilder()
-    .setTitle('Express delivery')
+    .setTitle('Ultradoc')
     .setDescription('Ultra backend api docs')
     .setVersion('1.0')
     .build();
